0fdb848d61f1989d9fe712644d8093fade370132
Name <reva00@ngs.ru>
2019-11-04 18:54:19 +0700

    version 2

--- BraceChecker.cpp
+++ BraceChecker.cpp
@@ -2,8 +2,9 @@
 
 bool BraceChecker::isBalanced(const std::string& input)
 {
-	return !(std::accumulate(input.begin(), input.end(), double(1), [] (double x, char y)
+	try{return !(std::accumulate(input.begin(), input.end(), double(1), [] (double x, char y)
 		{
+			if (x < 1) { throw 1;}
 			if (y == '{') { return x*2; }
 			if (y == '}') { return x/2; }
 			if (y == '[') { return x*3; }
@@ -11,5 +12,9 @@ bool BraceChecker::isBalanced(const std::string& input)
 			if (y == '(') { return x*5; }
 			if (y == ')') { return x/5; }
 			else { return x; }
-		}) - 1);
+		}) - 1);}
+	catch (int msg)
+	{
+		return 0;
+	}
 }
